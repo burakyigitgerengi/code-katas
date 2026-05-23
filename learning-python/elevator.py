721d5fa957d73003b882e907f22c4f065132cf90
Burak Gerengi <burakyigitgerengi@gmail.com>
2026-05-23 23:44:03 +0300

    [revision] code revision

--- learning-python/elevator.py
+++ learning-python/elevator.py
@@ -5,25 +5,24 @@ elevator_floor = 0
 user_floor = 0
 
 
-def get_floor_of_user():
-    global user_floor
-    floor_of_user = input("At which floor are you? (0-14): ")
-    try:
-        floor_of_user = int(floor_of_user)
-        if 0 <= floor_of_user <= 14:
-            user_floor = floor_of_user
-        else:
-            raise ValueError
-    except ValueError:
-        print("You must enter a valid number.")
-
-
 class Elevator:
     def __init__(self):
         if elevator_floor != user_floor:
             print(f"*The display shows the elevator is at floor {elevator_floor}*")
-        else:
-            print(f"*The elevator is already here at floor {elevator_floor}*")
+
+    def get_user_floor_input(self):
+        global user_floor
+        while True:
+            try:
+                user_floor_input = input("At which floor are you? (0-14): ")
+                user_floor_input = int(user_floor_input)
+                if 0 <= user_floor_input <= 14:
+                    user_floor = user_floor_input
+                    break
+                else:
+                    raise ValueError("You must enter a valid number.")
+            except ValueError:
+                print("You must enter a valid number.")
 
     def call(self):
         global elevator_floor
@@ -34,21 +33,22 @@ class Elevator:
 
     def select_floor(self):
         global elevator_floor
-        selected_floor = input("Type the floor you want to move to (0-14): ")
-        try:
-            selected_floor = int(selected_floor)
-            if 0 <= selected_floor <= 14:
-                elevator_floor = selected_floor
-                print(f"Ding! You have arrived at floor {elevator_floor}.")
-            else:
-                raise ValueError
-        except ValueError:
-            print("You must enter a valid number.")
+        while True:
+            try:
+                selected_floor = input("Type the floor you want to move to (0-14): ")
+                selected_floor = int(selected_floor)
+                if 0 <= selected_floor <= 14:
+                    elevator_floor = selected_floor
+                    print(f"Ding! You have arrived at floor {elevator_floor}.")
+                else:
+                    raise ValueError("You must enter a valid number.")
+            except ValueError:
+                print("You must enter a valid number.")
 
 
 def main():
-    get_floor_of_user()
     elevator = Elevator()
+    elevator.get_user_floor_input()
     elevator.call()
 
 
