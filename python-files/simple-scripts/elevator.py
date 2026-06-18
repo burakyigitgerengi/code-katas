231c8805b3d1689cdbc17a599f0c181b24b3c5d4
Burak Gerengi <burakyigitgerengi@gmail.com>
2026-06-18 13:16:15 +0300

    [improve] improve elevator.py

--- python-files/simple-scripts/elevator.py
+++ python-files/simple-scripts/elevator.py
@@ -1,55 +1,59 @@
-MAX_FLOOR = 14
-MIN_FLOOR = 0
+import random
 
-elevator_floor = 0
-user_floor = 0
+
+class User:
+    def __init__(self) -> None:
+        self.current_floor = random.randint(0, 14)
+        print(f"You are currently at floor: {self.current_floor}.")
 
 
 class Elevator:
-    def __init__(self):
-        if elevator_floor != user_floor:
-            print(f"*The display shows the elevator is at floor {elevator_floor}*")
+    def __init__(self, user: User):
+        self.current_floor = random.randint(0, 14)
+        self._max_floor = 14
+        self._min_floor = 0
+        if self.current_floor != user.current_floor:
+            print(f"*The display shows the elevator is at floor {self.current_floor}*")
 
-    def get_user_floor_input(self):
-        global user_floor
-        while True:
-            try:
-                user_floor_input = input("At which floor are you? (0-14): ")
-                user_floor_input = int(user_floor_input)
-                if 0 <= user_floor_input <= 14:
-                    user_floor = user_floor_input
-                    break
-                else:
-                    raise ValueError("You must enter a valid number.")
-            except ValueError:
-                print("You must enter a valid number.")
+    @property
+    def max_floor(self):
+        return self._max_floor
 
-    def call(self):
-        global elevator_floor
+    @max_floor.setter
+    def max_floor(self, value: int):
+        self._max_floor = value
+
+    @property
+    def min_floor(self):
+        return self._min_floor
+
+    @min_floor.setter
+    def min_floor(self, value):
+        self._min_floor = value
+
+    def call(self, user: User):
         print("*You pressed the button to call the elevator*")
-        elevator_floor = user_floor  # The elevator moves to you
-        print(f"Elevator has arrived at floor {elevator_floor}.")
+        self.current_floor = user.current_floor  # The elevator moves to you
+        print(f"Elevator has arrived at floor {self.current_floor}.")
         self.select_floor()  # Use self to call methods within the class
 
     def select_floor(self):
-        global elevator_floor
         while True:
             try:
-                selected_floor = input("Type the floor you want to move to (0-14): ")
-                selected_floor = int(selected_floor)
+                selected_floor = int(
+                    input("Type the floor you want to move to (0-14): ")
+                )
                 if 0 <= selected_floor <= 14:
-                    elevator_floor = selected_floor
-                    print(f"Ding! You have arrived at floor {elevator_floor}.")
-                else:
-                    raise ValueError("You must enter a valid number.")
+                    self.current_floor = selected_floor
+                    print(f"Ding! You have arrived at floor {self.current_floor}.")
             except ValueError:
-                print("You must enter a valid number.")
+                return "You must enter a valid number."
 
 
 def main():
-    elevator = Elevator()
-    elevator.get_user_floor_input()
-    elevator.call()
+    user = User()
+    elevator = Elevator(user)
+    elevator.call(user)
 
 
 main()
