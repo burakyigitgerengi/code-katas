68768161ee0783bc556f0403007685ed191a0bb5
Burak Gerengi <burakyigitgerengi@gmail.com>
2026-06-21 17:58:34 +0300

    [finish] binary-tree.py

--- DSA/binary-tree.py
+++ DSA/binary-tree.py
@@ -5,7 +5,7 @@ class Node:
         self.right = None
 
 
-class BinarySearchTree:
+class BinaryTree:
     def __init__(self) -> None:
         self.root = None
 
@@ -13,31 +13,53 @@ class BinarySearchTree:
         if self.root == None:
             self.root = Node(data)
         else:
-            current_node = self.root
-            while True:
-                if data < current_node.data:
-                    if not current_node.left:
-                        current_node.left = Node(data)
-                        return
-                    else:
-                        current_node = current_node.left
+            temp_list = [self.root]
+            while temp_list:
+                current_node = temp_list.pop(0)
+                if not current_node.left:
+                    current_node.left = Node(data)
+                    return
+                elif current_node.left:
+                    temp_list.append(current_node.left)
 
-                else:
-                    if current_node.right is None:
-                        current_node.right = Node(data)
-                        return
-                    else:
-                        current_node = current_node.right
+                if not current_node.right:
+                    current_node.right = Node(data)
+                    return
+                elif current_node.right:
+                    temp_list.append(current_node.right)
 
     def search(self, data):
-        current_node = self.root
-        while True:
-            if current_node.data == data:
-                return True
-            elif data < current_node.data and current_node.left:
-                if current_node.left:
-                    current_node = current_node.left
-            elif data > current_node.data and current_node.right:
-                current_node = current_node.right
-            else:
-                return False
+        temp_list = self.in_order_traversal()
+        return temp_list.__contains__(data)
+
+    def in_order_traversal(self):
+        """The main method called by the user."""
+        traversal_list = []
+
+        # Start the recursion from the root
+        self._in_order_helper(self.root, traversal_list)
+
+        return traversal_list
+
+    def _in_order_helper(self, current_node, traversal_list):
+        """The recursive helper method that does the heavy lifting."""
+        # Base Case: If the node is None, there is nothing to do. Just return.
+        if current_node is None:
+            return
+
+        # 1. GO LEFT
+        self._in_order_helper(current_node.left, traversal_list)
+
+        # 2. VISIT
+        traversal_list.append(current_node.data)
+
+        # 3. GO RIGHT
+        self._in_order_helper(current_node.right, traversal_list)
+
+    def find_max(self):
+        traversal_list = self.in_order_traversal()
+        return max(traversal_list)
+
+    def fin_min(self):
+        traversal_list = self.in_order_traversal()
+        return min(traversal_list)
